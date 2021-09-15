d6ac2b54d41da363b3199dce896083a4e227bf92
Sir-Unkie <unknown920@gmail.com>
2021-09-15 13:33:51 +0300

    added a color circle

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
 import styles from './App.module.scss';
-import { useEffect, useState, useCallback } from 'react';
-import { fetchData } from './utils/utils';
-import Circle from './Components/Circle/Circle';
+import { useEffect, useState } from 'react';
+import * as d3 from 'd3';
 
 const URL = `https://gist.githubusercontent.com/Sir-Unkie/8e7a83acaab32c3f6aa6224aea3c3b6c/raw/Rawdata.csv`;
 
 function App() {
-  const [data, setData] = useState();
-  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
-  const mouseMoveHandler = useCallback(
-    e => {
-      setCoordinates({ x: e.clientX, y: e.clientY });
-    },
-    [setCoordinates]
-  );
+  const [data, setData] = useState([]);
+  const width = '500';
+  const height = '500';
+  const centerX = width / 2;
+  const centerY = height / 2;
+  const pieArc = d3.arc();
+
   useEffect(() => {
     const getData = async () => {
-      const d = await fetchData(URL);
-      setData(d);
+      const response = await fetch(URL);
+      const data = await response.text();
+      const dataParsed = d3.csvParse(data);
+      // console.log(dataParsed);
+      setData(dataParsed);
     };
     getData();
   }, []);
   return (
-    <div className={styles.App} onMouseMove={mouseMoveHandler}>
-      <Circle x={coordinates.x} y={coordinates.y}></Circle>
+    <div className={styles.App}>
+      <svg width={width} height={height}>
+        <g transform={`translate(${centerX},${centerY})`}>
+          {data.map((element, i) => {
+            return (
+              <path
+                key={element.Keyword}
+                fill={element['RGB hex value']}
+                d={pieArc({
+                  innerRadius: 0,
+                  outerRadius: width,
+                  startAngle: (i / data.length) * 2 * Math.PI,
+                  endAngle: ((i + 1) / data.length) * 2 * Math.PI,
+                })}
+              />
+            );
+          })}
+        </g>
+      </svg>
     </div>
   );
 }
--- src/Components/Circle/Circle.jsx
+++ src/Components/Circle/Circle.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Circle = ({ x, y }) => {
   return (
     <svg width={'100vw'} height={'100vh'}>
-      <circle cx={x} cy={y} r={40} />
+      <circle cx={x} cy={y} r={10} />
     </svg>
   );
 };
